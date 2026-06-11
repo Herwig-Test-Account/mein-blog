24fa04d97a33dc48b835b379244c2577f236387a
Herwig-Test-Account <herwig.sauberer@gmail.com>
2026-06-11 18:29:11 +0200

    TinaCMS installiert und konfiguriert

---
+++ tina/config.ts
@@ -0,0 +1,74 @@
+import { defineConfig } from "tinacms";
+
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "main";
+
+export default defineConfig({
+  branch,
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  token: process.env.TINA_TOKEN,
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "static",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "static",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        format: "md",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Titel",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "datetime",
+            name: "date",
+            label: "Datum",
+            required: true,
+          },
+          {
+            type: "boolean",
+            name: "draft",
+            label: "Entwurf",
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Beschreibung (SEO)",
+            ui: {
+              component: "textarea",
+            },
+          },
+          {
+            type: "string",
+            name: "tags",
+            label: "Tags",
+            list: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Inhalt",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
